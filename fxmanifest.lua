8c5a3f88f60d5528e4a66128d23cba842e1f6312
Loxi <123603348+Loxi9999@users.noreply.github.com>
2024-07-18 00:49:52 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'https://tekken.dev'
+description 'FiveM Loading Screen'
+
+files {
+    "config.js",
+    "index.html",
+    "assets/**/*.*"
+}
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
